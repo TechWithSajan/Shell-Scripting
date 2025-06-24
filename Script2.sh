571651c9000400bcd395950b84cae1adc645a0d9
sajan <techdata@gmail.com>
2025-06-24 19:12:28 +0530

    1234

--- Script2.sh
+++ Script2.sh
@@ -8,6 +8,7 @@ echo "Hello value of a is $a and value of b is $b"
 echo "Hello we are lerning git and github"
 echo "I am Adding this line in branch-1" 
 echo "We are adding data in branch-2" 
+echo "TechData"-"sajan" 
 
 
 
