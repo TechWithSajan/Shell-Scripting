9b63895f5f58392127a66beef2a3b03b6d8e699d
sajan <techdata@gmail.com>
2025-12-28 15:09:07 +0530

    This is my first commit

--- script1.sh
+++ script1.sh
@@ -1,5 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from TDI and learning git/gihub"
 echo "I am from Pune"
-echo "We are form TDI-devosp-Batch"
-echo "hello Good evining" 
+echo "We are form TDI-devops_Sep-Batch"
+echo "hello Good evining" !!
