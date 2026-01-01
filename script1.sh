9928d3c6d9fdad6882a3358f0459dc8445c8a24f
Sajan Nikam <sajannikam111@gmail.com>
2026-01-01 20:08:18 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -2,5 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are from TDI and learning git/gihub"
 echo "I am from Pune"
 echo "We are form TDI-devops_Sep-Batch"
-echo "hello Good evining" !!
-echo "this is me"
+
